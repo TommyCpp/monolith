101dffa0702f295e2d6c038e3d2eebcee5e8802a
zhongyang.wu <zhongyang.wu@outlook.com>
2019-10-01 11:53:44 -0400

    add Chuck, add test

--- src/common.rs
+++ src/common.rs
@@ -6,14 +6,43 @@ pub type Value = f64;
 pub struct TimePoint {
     timestamp: Timestamp,
     value: Value,
-    metadata: BTreeMap<String, String>,
 
 //    How do we organize the metadata
 }
 
 impl TimePoint {
     pub fn new(timestamp: f64, value: f64) -> TimePoint {
-        let map: BTreeMap<String, String> = BTreeMap::new();
-        TimePoint { timestamp, value, metadata: map }
+        TimePoint { timestamp, value }
+    }
+}
+
+pub struct Chuck {
+    timestamps: Vec<TimePoint>
+}
+
+impl Chuck {
+    pub fn new() -> Chuck {
+        let timestamps = Vec::new();
+
+        Chuck { timestamps }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::common::*;
+
+    #[test]
+    fn create_chuck() {
+        let chuck = Chuck::new();
+        assert_eq!(chuck.timestamps.len(), 0)
+    }
+
+    #[test]
+    fn create_timepoint() {
+        let timepoint = TimePoint::new(120.0, 12.0);
+        assert_eq!(timepoint.timestamp, 120.0);
+        assert_eq!(timepoint.value, 12.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod common;
 use common::Timestamp;
 use crate::common::TimePoint;
 
+
 fn main() {
     println!("Hello, world!");
     let t: TimePoint = TimePoint::new(12.0, 12.0);
